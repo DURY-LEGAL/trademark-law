0a4eb595900564e6722e371bdb87e25e2a8419a5
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:40:47 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-law - Trademark law toolkit - Legal analysis and compliance tools. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
